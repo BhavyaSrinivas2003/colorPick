afdd8a2773c52f49654fa265a11c4d7786cd794b
Bhavya Srinivas <127350976+BhavyaSrinivas2003@users.noreply.github.com>
2023-10-22 08:37:11 +0530

    Add files via upload

---
+++ colorPick.js
@@ -0,0 +1,19 @@
+function button1On() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+}
+
+function button2On() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+}
+
+function button3On() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+}
+
+function button4On() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+}
